ec7733e45bc9c2546899bba3c891a88f72be6d23
aashowang <405294278@qq.com>
2015-08-03 14:55:30 +0800

    Initial Commit

---
+++ TTSDemoSepcPjt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTSDemoSepcPjt
+//
+//  Created by TangTieshan on 15/8/3.
+//  Copyright (c) 2015年 TangTieshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
